06e7d889c4fe1ce450e937f75ff7be40e528814a
ELIAS EMPRESAS <eliasempresas0@gmail.com>
2025-03-06 19:15:43 -0300

    modificações no código, BOTÃO A+B

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 enum RadioMessage {
-    message1 = 49434,
     INICIAR = 120,
     BOTAO_A = 1598,
     BOTAO_A_B = 4412,
     DIREITA = 12197,
     ESQUERDA = 27293,
     DESLIGAR = 30426,
-    BOTAO_B = 38770
+    BOTAO_B = 38770,
+    message1 = 49434
 }
 radio.onReceivedMessage(RadioMessage.BOTAO_A, function () {
     pins.analogWritePin(AnalogPin.P0, 1)
@@ -15,16 +15,66 @@ radio.onReceivedMessage(RadioMessage.BOTAO_A, function () {
     pins.analogWritePin(AnalogPin.P3, 0)
 })
 radio.onReceivedMessage(RadioMessage.DIREITA, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.analogWritePin(AnalogPin.P0, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
+    pins.analogWritePin(AnalogPin.P2, 1)
     pins.analogWritePin(AnalogPin.P3, 0)
 })
 radio.onReceivedMessage(RadioMessage.INICIAR, function () {
-    pins.analogWritePin(AnalogPin.P0, 1023)
-    pins.analogWritePin(AnalogPin.P1, 1023)
-    pins.analogWritePin(AnalogPin.P2, 1023)
-    pins.analogWritePin(AnalogPin.P3, 1023)
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 0)
+    }
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1)
+    pins.analogWritePin(AnalogPin.P3, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 0)
+    }
+    pins.analogWritePin(AnalogPin.P0, 1)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    pins.analogWritePin(AnalogPin.P3, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 0)
+    }
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 1)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 1)
+    }
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1)
+    pins.analogWritePin(AnalogPin.P3, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 0)
+    }
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 1)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 1)
+    }
+    for (let index = 0; index < 2; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P3, 0)
+    }
 })
 radio.onReceivedMessage(RadioMessage.BOTAO_B, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -39,15 +89,18 @@ radio.onReceivedMessage(RadioMessage.DESLIGAR, function () {
     pins.analogWritePin(AnalogPin.P3, 0)
 })
 radio.onReceivedMessage(RadioMessage.ESQUERDA, function () {
-    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
     pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 1)
+    pins.analogWritePin(AnalogPin.P2, 0)
     pins.analogWritePin(AnalogPin.P3, 0)
 })
 radio.onReceivedMessage(RadioMessage.BOTAO_A_B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.analogWritePin(AnalogPin.P1, 1023)
-    pins.analogWritePin(AnalogPin.P2, 1023)
-    pins.analogWritePin(AnalogPin.P3, 1023)
+    for (let index = 0; index < 5; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P3, 0)
+        continue;
+    }
 })
 radio.setGroup(55)
